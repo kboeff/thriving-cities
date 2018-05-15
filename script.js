e842da845ec1171efaffdca229417979108e7325
kboeff <kamen.boev@gmail.com>
2018-05-15 17:02:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var myVideo = document.getElementById("video1");
+
+// TODO: transform functions into responsive actions
+function makeBig() {
+	myVideo.width = 560;
+	console.log(myVideo);
+}
+
+function makeSmall() {
+	myVideo.width = 320;
+	console.log(myVideo);
+}
+
+function makeNormal() {
+	myVideo.width = 480;
+	console.log(myVideo);
+}
