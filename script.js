324e0e275a767a2fa4b09cf708bda8aa1e2f77d3
Kamen <kamen.boev@gmail.com>
2018-05-31 15:05:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-var myVideo = document.getElementById("video1");
+// Get the modal
+let modal = document.getElementById('myModal');
 
-// TODO: transform functions into responsive actions
-function makeBig() {
-	myVideo.width = 560;
-	console.log(myVideo);
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+let img = '';
+var modalImg = document.getElementById("event1");
+var captionText = document.getElementById("caption");
+const getId = (id) => {
+	img = document.getElementById(id);
+	modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
+    console.log(modal.style.display, modalImg, captionText.innerHTML)
+	console.log(id);
 }
 
-function makeSmall() {
-	myVideo.width = 320;
-	console.log(myVideo);
-}
-
-function makeNormal() {
-	myVideo.width = 480;
-	console.log(myVideo);
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
 }
